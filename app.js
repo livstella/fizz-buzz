52ac01402b9fc8b5a66a12e44f2174329c15b346
Liv Reinhold <livreinhold@gmail.com>
2022-01-17 10:04:47 +0100

    small update in function for better functionality

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let numberArray=[1,2,3,4,5,6,7,8,9,10,12,15];
 
 function fizzBuzz(anArray){
-   for (let i=0; i<anArray.length; i++ ){
+   for ( i=0; i<anArray.length; i++ ){
        if(anArray[i]%3==0 && anArray[i]%5==0){
             console.log("FizzBuzz")
         }else if (anArray[i]%3==0){
-            console.log("fizz")
+            console.log("Fizz")
         }else if(anArray[i]%5==0){
             console.log("Buzz")
         }else{
